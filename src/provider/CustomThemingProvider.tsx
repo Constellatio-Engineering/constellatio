f8357df300ef582b358cf03151998ef0f9967c96
Ahmed Ghazy <a.ghazy@cyclus.digital>
2023-07-12 18:37:33 +0300

    finished dropdown

--- src/provider/CustomThemingProvider.tsx
+++ src/provider/CustomThemingProvider.tsx
@@ -1,41 +1,58 @@
 import { colors } from "@/constants/styles/colors";
 import { radius } from "@/constants/styles/radius";
 import { spacing } from "@/constants/styles/spacing";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, MantineTheme, ScrollAreaStylesNames, ScrollAreaStylesParams, Styles } from "@mantine/core";
 import React, { ReactNode } from "react";
 
-const CustomThemingProvider = ({ children }: { children: ReactNode }) => (
-  <MantineProvider
-    withGlobalStyles
-    withNormalizeCSS
-    theme={{
-      colorScheme: "light",
-      fontFamily: "Karla, sans-serif",
-      headings: {
-        fontFamily: "Libre Baskerville, serif",
-        fontWeight: 700,
-        sizes: {
-          h1: {
-            fontSize: "44px",
-            lineHeight: "68px",
-          },
-          h2: {
-            fontSize: "28px",
-            lineHeight: "40px",
+const CustomThemingProvider = ({ children }: { children: ReactNode }) => {
+  const scrollAreaStyles: Styles<ScrollAreaStylesNames, ScrollAreaStylesParams> = (theme: MantineTheme) => ({
+    scrollbar: {
+      backgroundColor: theme.colors["neutrals-01"][0],
+    },
+    thumb: {
+      borderRadius: theme.radius["full"],
+      backgroundColor: theme.colors["neutrals-01"][4],
+    },
+  });
+
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+        fontFamily: "Karla, sans-serif",
+        headings: {
+          fontFamily: "Libre Baskerville, serif",
+          fontWeight: 700,
+          sizes: {
+            h1: {
+              fontSize: "44px",
+              lineHeight: "68px",
+            },
+            h2: {
+              fontSize: "28px",
+              lineHeight: "40px",
+            },
           },
         },
-      },
-      colors: colors as any,
-      shadows: {
-        default: "0px 8px 32px 0px rgba(0, 0, 0, 0.08)",
-      },
-      radius,
-      spacing,
-      fontSizes: spacing,
-    }}
-  >
-    {children}
-  </MantineProvider>
-);
+        colors: colors as any,
+        shadows: {
+          default: "0px 8px 32px 0px rgba(0, 0, 0, 0.08)",
+        },
+        radius,
+        spacing,
+        fontSizes: spacing,
+        components: {
+          ScrollArea: {
+            styles: scrollAreaStyles,
+          },
+        },
+      }}
+    >
+      {children}
+    </MantineProvider>
+  );
+};
 
 export default CustomThemingProvider;
