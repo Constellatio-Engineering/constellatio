a692012058c280c4dc0bc860d7b4d9018a16c619
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-08-24 13:46:06 +0300

    Category tab hard coded dummy data

--- app/src/pages/test.tsx
+++ app/src/pages/test.tsx
@@ -3,12 +3,16 @@ import { getProps } from "@/services/content/getProps";
 import { GetStaticPaths, type GetStaticProps } from "next";
 import React from "react";
 
-const NextPage = (props) => 
+import CategoryTab from "../components/categoryTab/CategoryTab";
+import { CivilLawIcon } from "../components/Icons/CivilLawIcon";
+
+const NextPage = (props: any): any => 
 {
   console.log(props);
   return (
     <div>NextPage
       {/* <Box w={700}><DragDropGame game={props?.Page.components[3].game}/></Box> */}
+      <CategoryTab title="Category Tab" icon={<CivilLawIcon/>} itemsNumber={3}/>
     </div>
   );
 };
