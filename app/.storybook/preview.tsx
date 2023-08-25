06abfe228a973e0de228b84369c8a41a3fbe0770
Kotti <christian@dualmeta.io>
2023-08-25 22:56:21 +0200

    add linting for storybook folder

--- app/.storybook/preview.tsx
+++ app/.storybook/preview.tsx
@@ -1,95 +1,97 @@
 import type { Preview } from "@storybook/react";
 import React from "react";
+ 
 import CustomThemingProvider from "../src/provider/CustomThemingProvider";
 
 const customViewports = {
   Bronze: {
     name: "Bronze",
     styles: {
+      height: "100%",
       width: "640px",
-      height: "100%",
-    },
-  },
-  Silver: {
-    name: "Silver",
-    styles: {
-      width: "768px",
-      height: "100%",
-    },
-  },
-  Gold: {
-    name: "Gold",
-    styles: {
-      width: "1280px",
-      height: "100%",
-    },
-  },
-  Platinum: {
-    name: "Platinum",
-    styles: {
-      width: "1440px",
-      height: "100%",
     },
   },
   Diamond: {
     name: "Diamond",
     styles: {
-      width: "1920px",
       height: "100%",
+      width: "1920px",
     },
   },
-  iphoneX: {
-    name: "iPhone X",
+  Gold: {
+    name: "Gold",
     styles: {
-      width: "375px",
-      height: "812px",
+      height: "100%",
+      width: "1280px",
+    },
+  },
+  Platinum: {
+    name: "Platinum",
+    styles: {
+      height: "100%",
+      width: "1440px",
+    },
+  },
+  Silver: {
+    name: "Silver",
+    styles: {
+      height: "100%",
+      width: "768px",
     },
   },
   ipad: {
     name: "iPad",
     styles: {
-      width: "768px",
       height: "1024px",
+      width: "768px",
     },
   },
   ipadMini: {
     name: "iPad landscape",
     styles: {
-      width: "1024px",
       height: "768px",
+      width: "1024px",
+    },
+  },
+  iphoneX: {
+    name: "iPhone X",
+    styles: {
+      height: "812px",
+      width: "375px",
     },
   },
   pixel: {
     name: "Google Pixel",
     styles: {
-      width: "412px",
       height: "732px",
+      width: "412px",
     },
   },
 };
 
 const preview: Preview = {
+  decorators: [
+    (Story) => 
+    {
+      return (
+        <CustomThemingProvider>
+          <Story/>
+        </CustomThemingProvider>
+      );
+    },
+  ],
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    viewport: {
-      viewports: customViewports,
-    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
-  },
-  decorators: [
-    (Story) => {
-      return (
-        <CustomThemingProvider>
-          <Story />
-        </CustomThemingProvider>
-      );
+    viewport: {
+      viewports: customViewports,
     },
-  ],
+  },
 };
 
 export default preview;
