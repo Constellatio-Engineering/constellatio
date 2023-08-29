43d1fb17070919051e1980a2e5188f1146727699
Kotti <accounts@christiankottmann.me>
2023-08-29 15:23:43 +0200

    lint update

--- app/src/components/atoms/countLabel/CountLabel.styles.ts
+++ app/src/components/atoms/countLabel/CountLabel.styles.ts
@@ -1,14 +1,13 @@
 import { css } from "@emotion/react";
-import { MantineTheme } from "@mantine/styles";
-import { ICountLabel } from "./CountLabel";
+import { type MantineTheme } from "@mantine/styles";
 
-export const wrapper = ({
-  theme,
-  variant,
-}: {
+import { type ICountLabel } from "./CountLabel";
+
+export const wrapper = ({ theme, variant }: {
   theme: MantineTheme;
   variant: ICountLabel["variant"];
-}) => {
+}) => 
+{
   const color =
     variant === "success" || variant === "error"
       ? theme?.colors["neutrals-01"][0]
@@ -17,12 +16,12 @@ export const wrapper = ({
     variant === "success"
       ? theme?.colors["support-success"][3]
       : variant === "error"
-      ? theme?.colors["support-error"][3]
-      : variant === "cases"
-      ? theme?.colors["support-notice"][2]
-      : variant === "dictionary"
-      ? theme?.colors["cc-dictionary"][2]
-      : variant === "neutral" && theme?.colors["neutrals-01"][2];
+        ? theme?.colors["support-error"][3]
+        : variant === "cases"
+          ? theme?.colors["support-notice"][2]
+          : variant === "dictionary"
+            ? theme?.colors["cc-dictionary"][2]
+            : variant === "neutral" && theme?.colors["neutrals-01"][2];
 
   return css`
     padding: 4px 8px;
