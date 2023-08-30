9eaab7cfe8fbfbbb77f67c8d2b61d87c468b77e0
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-08-30 20:49:37 +0300

    overview header as a stand alone component

---
+++ app/src/components/organisms/casesOverviewHeader/CasesOverviewHeader.stories.tsx
@@ -0,0 +1,42 @@
+import { type Meta, type StoryObj } from "@storybook/react";
+
+import OverviewHeader, { type ICasesOverviewHeaderProps } from "./CasesOverviewHeader";
+
+const Template = (args: ICasesOverviewHeaderProps): JSX.Element => (<OverviewHeader {...args}/>);
+
+const meta: Meta = {
+  argTypes: {
+    
+  },
+  component: Template,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/KZhlH1AesOBZZf1V4F9d2r/Constellatio-%E2%80%93-UI-Kit?type=design&node-id=49-2862&mode=dev",
+    },
+  },
+  title: "Organisms/OverviewHeader",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof OverviewHeader>;
+
+export const Case: Story = {
+  args: {
+    // __typename: "PageHeader",
+    categories: [{ title: "Civil Law" }, { title: "Public Law" }, { title: "Criminal Law" }],
+    title: "Cases",
+    variant: "case"
+  },
+};
+
+export const Dictionary: Story = {
+  args: {
+    // __typename: "PageHeader",
+    categories: [{ title: "Civil Law" }, { title: "Public Law" }, { title: "Criminal Law" }],
+    title: "Dictionary",
+    variant: "dictionary"
+  },
+};
+
