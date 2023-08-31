62ca9bf8dd7e3f0525143e9912e673ad1d070624
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-08-31 14:02:59 +0300

    all case fragment and query buildup

---
+++ app/src/services/graphql/fragments/f_Case.ts
@@ -0,0 +1,52 @@
+
+import { gql } from "graphql-request";
+
+export const f_Case = gql`
+  fragment Case on Case {
+    __typename
+    _meta {
+      id
+    }
+    title
+    topic {
+      ... on Topic {
+        id
+        topicName
+      }
+    }
+    tags {
+      ... on Tags {
+        id
+        tagName
+      }
+    }
+    mainCategoryField {
+      ... on MainCategory {
+        mainCategory
+      }
+    }
+    subCategoryField {
+      ... on SubCategory {
+        subCategory
+        mainCategory {
+          ... on MainCategory {
+            mainCategory
+          }
+        }
+      }
+    }
+    legalArea {
+      ... on LegalArea {
+        id
+        legalAreaName
+      }
+    }
+    fullTextTasks {
+      json
+    }
+    fact {
+      json
+    }
+    durationToComplete
+  }
+`;
