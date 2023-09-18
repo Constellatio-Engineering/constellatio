fb3310fa700b70f3cccd06256ead420010f12eff
Kotti <christian@dualmeta.io>
2023-09-19 00:50:16 +0200

    basic search working

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Layout } from "@/components/layouts/Layout";
+import { env } from "@/env.mjs";
 import { getCommonProps } from "@/utils/commonProps";
 
+import { MeiliSearch } from "meilisearch";
 import { type GetServerSideProps } from "next";
 import { type SSRConfig } from "next-i18next";
-import { type FunctionComponent, useState } from "react";
+import { type FunctionComponent, useEffect, useState } from "react";
 
 import { defaultLocale } from "../../next.config.mjs";
 
@@ -21,15 +23,38 @@ export const getServerSideProps: GetServerSideProps<ServerSidePropsResult> = asy
   };
 };
 
+const meiliSearch = new MeiliSearch({
+  apiKey: env.NEXT_PUBLIC_MEILISEARCH_SEARCH_API_KEY,
+  host: env.NEXT_PUBLIC_MEILISEARCH_HOST
+});
+
+type Movie = {
+  id: number;
+  title: string;
+};
+
 const Home: FunctionComponent<ServerSidePropsResult> = () =>
 {
   const [input, setInput] = useState<string>("");
+  const [results, setResults] = useState<Movie[]>([]);
 
+  useEffect(() =>
+  {
+    meiliSearch.index("movies").search<Movie>(input)
+      .then((res) => setResults(res.hits))
+      .catch((err) => console.log(err));
+  }, [input]);
+  
   return (
     <Layout>
       <div style={{ padding: 100 }}>
         <h1>Search</h1>
         <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+        {results.map(result => (
+          <div key={result.id}>
+            <p>{result.title}</p>
+          </div>
+        ))}
       </div>
     </Layout>
   );
