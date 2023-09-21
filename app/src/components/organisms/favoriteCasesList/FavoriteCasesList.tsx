4e85385d5c6f2d701618543eb3c071d61c76d88b
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-09-21 17:33:37 +0300

    separating cases list comp

--- app/src/components/organisms/favoriteCasesList/FavoriteCasesList.tsx
+++ app/src/components/organisms/favoriteCasesList/FavoriteCasesList.tsx
@@ -1,41 +1,44 @@
+
+import { Svg } from "@/basic-components/SVG/Svg";
+import { type IGenArticleOverviewFragment, IGenCase, type IGenFullCaseFragment, type IGenMainCategory } from "@/services/graphql/__generated/sdk";
+
+import { Container } from "@mantine/core";
 import React, { type FunctionComponent } from "react";
 
 import * as styles from "./FavoriteCasesList.styles";
-import { Svg } from "@/basic-components/SVG/Svg";
-import { IGenArticleOverviewFragment, IGenCase, IGenFullCaseFragment, IGenMainCategory } from "@/services/graphql/__generated/sdk";
-import { Container } from "@mantine/core";
 import ItemBlock from "../caseBlock/ItemBlock";
 
 interface FavoriteCasesListProps
 {
-  bookmarkedCasesMainCategoriesUnique: IGenMainCategory[];
-  casesByMainCategory:(mainCategoryId: string) => (({ _typename?: "Case" | undefined; } & IGenFullCaseFragment) | null | undefined)[] | IGenArticleOverviewFragment[] | undefined
+  readonly bookmarkedCasesMainCategoriesUnique: IGenMainCategory[];
+  readonly casesByMainCategory: (mainCategoryId: string) => Array<({ _typename?: "Case" | undefined } & IGenFullCaseFragment) | null | undefined> | IGenArticleOverviewFragment[] | undefined;
 }
 
-const FavoriteCasesList: FunctionComponent<FavoriteCasesListProps> = ({ bookmarkedCasesMainCategoriesUnique, casesByMainCategory }) => {
+const FavoriteCasesList: FunctionComponent<FavoriteCasesListProps> = ({ bookmarkedCasesMainCategoriesUnique, casesByMainCategory }) => 
+{
   return (
     <Container maw={1440}>
-    {bookmarkedCasesMainCategoriesUnique.map((mainCategoryBlock: IGenMainCategory, blockIndex: number) => (
-      <React.Fragment key={blockIndex}>
-        <ItemBlock
-          variant="case"
-          blockHead={{
-            blockType: "facouritItemsBlock", 
-            categoryName: mainCategoryBlock?.mainCategory ?? "", 
-            completedCases: 999, 
+      {bookmarkedCasesMainCategoriesUnique.map((mainCategoryBlock: IGenMainCategory, blockIndex: number) => (
+        <React.Fragment key={blockIndex}>
+          <ItemBlock
+            variant="case"
+            blockHead={{
+              blockType: "facouritItemsBlock", 
+              categoryName: mainCategoryBlock?.mainCategory ?? "", 
+              completedCases: 999, 
         
-            icon: {
-              alt: mainCategoryBlock?.icon?.title ?? "",
-              src: <Svg src={mainCategoryBlock?.icon?.src}/>
-            },
-            items: casesByMainCategory(mainCategoryBlock?.id ?? "")?.length ?? 0,
-            variant: "case"
-          }}
-          items={casesByMainCategory(mainCategoryBlock?.id ?? "")}
-        />
-      </React.Fragment>
-    ))}
-  </Container>
+              icon: {
+                alt: mainCategoryBlock?.icon?.title ?? "",
+                src: <Svg src={mainCategoryBlock?.icon?.src}/>
+              },
+              items: casesByMainCategory(mainCategoryBlock?.id ?? "")?.length ?? 0,
+              variant: "case"
+            }}
+            items={casesByMainCategory(mainCategoryBlock?.id ?? "")}
+          />
+        </React.Fragment>
+      ))}
+    </Container>
   );
 };
 
