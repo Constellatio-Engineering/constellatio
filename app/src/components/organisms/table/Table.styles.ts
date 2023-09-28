5df0bfa32f7ca726fe0f021941194a526014417f
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-09-28 17:00:47 +0300

    dictionary table

--- app/src/components/organisms/table/Table.styles.ts
+++ app/src/components/organisms/table/Table.styles.ts
@@ -1,10 +1,15 @@
 import { css } from "@emotion/react";
 
 export const table = css`
+.primaryCell{
+	width:100%;
+}
 	width: 100%;
 	vertical-align: middle;
 
 	> * {
+		white-space: nowrap;
+		min-width: max-content;
 		vertical-align: middle;
 	}
 `;
