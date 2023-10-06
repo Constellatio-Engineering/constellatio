a21b3623c7bc77b626130ad7848b0353bcf76a69
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-06 22:59:27 +0300

    enter to submit to modals + header fixed position content fix

--- app/src/components/layouts/Layout.styles.ts
+++ app/src/components/layouts/Layout.styles.ts
@@ -2,7 +2,9 @@ import { type SerializedStyles, css } from "@emotion/react";
 import { type MantineTheme } from "@mantine/styles";
 
 export const wrapper = (theme: MantineTheme): SerializedStyles => css`
-/* max-width: 1440px; */
 margin: 0 auto;
 background-color: ${theme.colors["neutrals-01"][2]};
+`;
+export const main = css`
+    margin-top:60px;
 `;
