1929441ec7befc7d07959d2d41718cbfab17e75e
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-09 21:20:16 +0300

    table style fixes

--- app/src/components/organisms/uploadedMaterialTable/UploadedMaterialTable.styles.ts
+++ app/src/components/organisms/uploadedMaterialTable/UploadedMaterialTable.styles.ts
@@ -8,7 +8,7 @@ export const tableWrapper = (theme: MantineTheme) => css`
   text-align: left;
   border-radius: 12px;
   overflow: hidden;
-  outline: 1px solid ${theme.colors["neutrals-01"][1]};
+  outline: 1px solid ${theme.colors["neutrals-01"][3]};
   .mantine-Menu-dropdown {
     padding: 0;
     border-radius: 12px; 
@@ -31,7 +31,7 @@ export const tableWrapper = (theme: MantineTheme) => css`
     white-space: nowrap;
   }
   tr {
-    border: 1px solid ${theme.colors["neutrals-01"][2]};
+    border: 1px solid ${theme.colors["neutrals-01"][3]};
   }
   .primaryCell {
     width: 100%;
