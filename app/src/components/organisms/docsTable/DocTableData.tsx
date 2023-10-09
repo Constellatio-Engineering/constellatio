ec651f5a4dfedc86b9adef61d0ff46bad65ec97f
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-09 16:01:18 +0300

    doc delete modal fix

--- app/src/components/organisms/docsTable/DocTableData.tsx
+++ app/src/components/organisms/docsTable/DocTableData.tsx
@@ -98,7 +98,16 @@ export const DocsTableData: FunctionComponent<Document> = (doc) =>
             <Menu.Divider/>
             <Menu.Item><DropdownItem icon={<DownloadIcon/>} label="Download" onClick={download}/></Menu.Item>
             <Menu.Divider/>
-            <Menu.Item><DropdownItem icon={<Trash/>} label="Download" onClick={() => deleteDocument({ id: documentId })}/></Menu.Item>
+            <Menu.Item>
+              <DropdownItem
+                icon={<Trash/>}
+                label="Delete"
+                onClick={() => 
+                {
+                  setShowDeleteDocModal(true);
+                }}
+              />
+            </Menu.Item>
           </Menu.Dropdown>
         </Menu>
         {/* Modal */}
@@ -112,8 +121,8 @@ export const DocsTableData: FunctionComponent<Document> = (doc) =>
           <span className="close-btn" onClick={() => setShowDeleteDocModal(false)}>
             <Cross size={32}/>
           </span>
-          <Title order={3}>Delete folder</Title>
-          <BodyText styleType="body-01-regular" component="p" className="delete-folder-text">Are you sure you want to delete <strong>Folder name</strong>?</BodyText>
+          <Title order={3}>Delete Document</Title>
+          <BodyText styleType="body-01-regular" component="p" className="delete-folder-text">Are you sure you want to delete document <strong>{doc?.name}</strong>?</BodyText>
           <div className="modal-call-to-action">
             <Button<"button">
               styleType={"secondarySimple" as TButton["styleType"]}
@@ -124,6 +133,7 @@ export const DocsTableData: FunctionComponent<Document> = (doc) =>
               styleType="primary"
               onClick={() => 
               {
+                deleteDocument({ id: documentId });
                 setShowDeleteDocModal(false);
               }}>
               Yes, Delete
