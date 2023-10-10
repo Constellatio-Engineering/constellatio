e81f1474ab2cbe9d3b03c4de2d4b0029be15ba49
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-10 18:31:26 +0300

    TODO COMMENT

--- app/src/components/profileBadgesBlockList/ProfileBadgesBlockList.tsx
+++ app/src/components/profileBadgesBlockList/ProfileBadgesBlockList.tsx
@@ -8,12 +8,18 @@ const ProfileBadgesBlockList: FunctionComponent = () =>
   const badges = [
     { description: "You provided feedback to improve design or content", name: "badge1" }, 
     { description: "You provided feedback to improve design or content", name: "badge2" },
+    { description: "You provided feedback to improve design or content", name: "badge2" },
+    { description: "You provided feedback to improve design or content", name: "badge2" },
+    { description: "You provided feedback to improve design or content", name: "badge2" },
+    { description: "You provided feedback to improve design or content", name: "badge2" },
+    { description: "You provided feedback to improve design or content", name: "badge2" },
     { description: "You provided feedback to improve design or content", name: "badge3" }
   ];
   return (
     <div css={styles.wrapper}>
       {
-        badges?.map((badge, badgeIndex) => (
+      // TODO CREATE SHOW ALL BUTTON FOR WHEN LENGTH IS MORE THAN 9
+        badges?.slice(0, 9)?.map((badge, badgeIndex) => (
           <Fragment key={badgeIndex}>
             <ProfileBadgeCard name={badge.name} description={badge.description}/>
           </Fragment>
