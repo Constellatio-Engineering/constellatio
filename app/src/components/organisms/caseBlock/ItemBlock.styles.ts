d1f247e392020dc5177fca0771ee71d72e2b72fe
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-12 19:16:59 +0300

    expand overview table logic

--- app/src/components/organisms/caseBlock/ItemBlock.styles.ts
+++ app/src/components/organisms/caseBlock/ItemBlock.styles.ts
@@ -1,11 +1,12 @@
 import { css } from "@emotion/react";
+import { type MantineTheme } from "@mantine/styles";
 
 export const wrapper = css`
 padding:32px;
 border-radius: 8px;
 background-color: #fff;
 display: flex;
-justify-content: flex-start;
+justify-content: stretch;
 align-items: flex-start;    
 gap: 32px;
 margin:24px 0;
@@ -13,4 +14,43 @@ position:relative;
 z-index: 3;
 /* color doesn't exist in Mantine Theme Colors array */
 box-shadow: 0px 8px 44px 0px #0000000a;
+span > table {
+    width:100%;
+}
+`;
+export const expandTableButton = css`
+    margin: 0 auto;
+    display: block;
+
+`;
+const CSSCommonExpandButtonStyles = (theme: MantineTheme) => css`
+    content: "";
+    position: absolute;
+    width: 38%;
+    background: ${theme.colors["neutrals-01"][3]};
+    height: 3px;
+    top: 50%;
+`;
+
+export const expandTableButtonArea = (theme: MantineTheme) => css`
+background-color: ${theme.colors["neutrals-01"][0]};
+    position: relative;
+.linearGredient{
+    content: "";
+    position: absolute;
+    width: 100%;
+    background:linear-gradient(to bottom, transparent 0%,${theme.colors["neutrals-01"][0]} 100%) ;
+    height: 350%;
+    top: -350%;
+    left: 0;
+}
+&::after{
+${CSSCommonExpandButtonStyles(theme)}
+}
+&::before{
+    
+right: 0;
+
+    ${CSSCommonExpandButtonStyles(theme)}
+}
 `;
