7424a0fd3193580cdc4e9bbebe469d29b08d7d3e
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-17 17:33:37 +0300

    drag and drop game loader fix

--- app/src/components/organisms/FillGapsGame/FillGapsGame.tsx
+++ app/src/components/organisms/FillGapsGame/FillGapsGame.tsx
@@ -51,7 +51,6 @@ let FillGapsGame: FC<TFillGapsGame> = ({
   const updateGameState = useFillGapsGameStore((s) => s.updateGameState);
   const initializeNewGameState = useFillGapsGameStore((s) => s.initializeNewGameState);
   const checkAnswers = useFillGapsGameStore((s) => s.checkAnswers);
-
   useEffect(() => 
   {
     if(gameState == null && id != null) 
@@ -72,7 +71,6 @@ let FillGapsGame: FC<TFillGapsGame> = ({
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameState?.correctAnswers]);
-
   const userEntriesArr = useMemo(() => 
   {
     if(!gameState?.userAnswers) 
@@ -84,7 +82,6 @@ let FillGapsGame: FC<TFillGapsGame> = ({
       []
     );
   }, [gameState?.userAnswers]);
-
   const richtextOverwrite = useCallback(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (props: any): ReactElement => 
