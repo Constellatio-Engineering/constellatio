98cc9427c7c69adf818248b7faf6b3b1126d0627
Hassan Mostafa <h.mostafa@cyclus.digital>
2023-10-24 12:06:55 +0300

    change password form updates

--- app/src/components/organisms/changePasswordTab/ChangePasswordTab.tsx
+++ app/src/components/organisms/changePasswordTab/ChangePasswordTab.tsx
@@ -1,25 +1,33 @@
 
+import { Button } from "@/components/atoms/Button/Button";
 import { AlertCard } from "@/components/atoms/Card/AlertCard";
+import { Input } from "@/components/atoms/Input/Input";
+import { PasswordValidationSchema } from "@/components/helpers/PasswordValidationSchema";
+import { updatePasswordFormSchema, type UpdatePasswordFormSchema } from "@/schemas/auth/updatePasswordForm.schema";
 
-import { Input, TextInput, Title } from "@mantine/core";
-import { useForm } from "@mantine/form";
+import { Title } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
 import React, { type FunctionComponent } from "react";
 
-import { BodyText } from "../../atoms/BodyText/BodyText";
-import { Button } from "../../atoms/Button/Button";
 import * as styles from "../profileDetailsTab/ProfileDetailsTab.styles";
+
+const initialValues: UpdatePasswordFormSchema = {
+  password: "",
+  passwordConfirm: "",
+};
 
 const ChangePasswordTab: FunctionComponent = () => 
 {
   const form = useForm({
-    initialValues: {
-      confirmPassword: "",
-      currentPassword: "",
-      newPassword: "",
-    },
+    initialValues,
+    validate: zodResolver(updatePasswordFormSchema),
+    validateInputOnBlur: true,
+
   });
   const [err, setErr] = React.useState<boolean>(true);
   const [success, setSuccess] = React.useState<boolean>(true);
+  const [isPasswordRevealed] = useDisclosure(false);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => 
   {
     e.preventDefault();
@@ -31,16 +39,28 @@ const ChangePasswordTab: FunctionComponent = () =>
       {err && <AlertCard onClick={() => setErr(false)} variant="error">Sorry, we weren not able to change password. Please, try again</AlertCard>}
       {success && <AlertCard onClick={() => setSuccess(false)} variant="success">Your password has been changed</AlertCard>}
       <form onSubmit={handleSubmit}>
-        <Input.Wrapper label={<BodyText styleType="body-01-regular">Current password</BodyText>} description="" error="">
-          <TextInput placeholder="" type="text" {...form.getInputProps("currentPassword")}/>
-        </Input.Wrapper>
-        <Input.Wrapper label={<BodyText styleType="body-01-regular">New password</BodyText>} description="" error="">
-          <TextInput placeholder="" type="text" {...form.getInputProps("newPassword")}/>
-        </Input.Wrapper>
-        <Input.Wrapper label={<BodyText styleType="body-01-regular">Confirm password</BodyText>} description="" error="">
-          <TextInput placeholder="" type="text" {...form.getInputProps("confirmPassword")}/>
-          <Button<"button"> type="submit" styleType="primary">Change password</Button>
-        </Input.Wrapper>
+        <Input
+          inputType="password"
+          label="Current password"
+          description="Just to make sure it is you and protect your account"
+          error="Sorry, your password doesn't match our records"
+        />
+        <Input
+          inputType="password" 
+          label="New password"
+          {...form.getInputProps("password")}
+        />
+        <PasswordValidationSchema
+          passwordValue={form.values.password}
+          isPasswordRevealed={isPasswordRevealed}
+        />
+        <Input 
+          {...form.getInputProps("passwordConfirm")}
+          inputType="password" 
+          label="Confirm password"
+          error={form.errors.passwordConfirm}
+        />
+        <Button<"button"> type="submit" styleType="primary" size="large">Change password</Button>
       </form>
     </div>
   );
