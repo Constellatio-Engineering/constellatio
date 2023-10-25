3b1e280f8e7e5ae18df9acb3670e034ed4dfd331
Ahmed Ghazy <ahmed.h.ghazy3@gmail.com>
2023-10-25 23:24:53 +0300

    fix liniting

--- app/src/utils/env.ts
+++ app/src/utils/env.ts
@@ -1,3 +1,4 @@
 import { env } from "@/env.mjs";
 
 export const isDevelopmentOrStaging = env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT === "development" || env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT === "staging";
+export const isDevelopment = env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT === "development";
