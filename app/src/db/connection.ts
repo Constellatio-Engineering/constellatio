c5da088aa6fbd289acc418e4c8d4aa1bfce76d75
Kotti <christian@dualmeta.io>
2023-10-27 10:21:51 +0200

    test

--- app/src/db/connection.ts
+++ app/src/db/connection.ts
@@ -4,6 +4,8 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import { env } from "../env.mjs";
 
+console.log("env.DATABASE_URL", env.DATABASE_URL);
+
 const connectionString = env.DATABASE_URL;
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
