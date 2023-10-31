0394a8788941b30a7caa6ac3fabf6a3959a9c24c
Kotti <christian@dualmeta.io>
2023-10-31 16:03:05 +0100

    small fixes

--- app/src/hooks/useBadges.ts
+++ app/src/hooks/useBadges.ts
@@ -1,14 +1,33 @@
 import { api } from "@/utils/api";
+import { type Path } from "@/utils/paths";
+
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 type Options = {
   disabled?: boolean;
 };
 
+export const disabledForPaths: Path[] = [
+  "/confirm",
+  "/paymentSuccess",
+  "/confirm-email-change",
+  "/recover",
+  "/register",
+  "/login",
+];
+
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const useBadges = (options?: Options) =>
 {
+  const router = useRouter();
+  const isDisabledForCurrentPath = useMemo(
+    () => disabledForPaths.some((path) => router.pathname.startsWith(path)),
+    [router.pathname]
+  );
+
   const { data, error, isLoading } = api.badges.getBadges.useQuery(undefined, {
-    enabled: !options?.disabled,
+    enabled: !isDisabledForCurrentPath && !options?.disabled,
     refetchOnMount: "always",
     refetchOnWindowFocus: "always",
     staleTime: Infinity
