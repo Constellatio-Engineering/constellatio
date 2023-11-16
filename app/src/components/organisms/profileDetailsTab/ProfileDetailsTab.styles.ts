90fb4bfd7f49af89d2e5c007e81ecbffd6cec6a5
Kotti <christian@dualmeta.io>
2023-11-16 20:49:05 +0100

    removed password validation / requirements

--- app/src/components/organisms/profileDetailsTab/ProfileDetailsTab.styles.ts
+++ app/src/components/organisms/profileDetailsTab/ProfileDetailsTab.styles.ts
@@ -2,56 +2,65 @@ import { css } from "@emotion/react";
 import { type MantineTheme } from "@mantine/styles";
 
 export const wrapper = (theme: MantineTheme) => css`
-        width: 440px;
-    .mantine-InputWrapper-root{
-    margin-bottom: 12px;
-    input, select,.mantine-PasswordInput-input{
-        border-radius: 8px;
-        height: 48px;
-        display: grid;
-        place-items: center;
+  width: 440px;
+
+  .mantine-InputWrapper-root {
+    input, select, .mantine-PasswordInput-input {
+      border-radius: 8px;
+      height: 48px;
+      display: grid;
+      place-items: center;
     }
-   .mantine-Input-rightSection{
-    svg{
+
+    .mantine-Input-rightSection {
+      svg {
         transform: translateY(6px);
+      }
     }
-   }
-   .mantine-InputWrapper-label{
-    width:100%;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-    span{
+
+    .mantine-InputWrapper-label {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      span {
         color: ${theme.colors["neutrals-01"][7]};
+      }
     }
-   }
-}
-h3{
+  }
+
+  h3 {
     margin-bottom: 24px;
-}
-form{
-    margin-top: 24px;
-    display:inline-block;
+  }
+
+  form {
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
     width: 100%;
+
     .mantine-Button-root {
-        margin-top: 24px;
-        width: 100%
-    } 
-}
-@media screen and (max-width: 1100px) {
-        margin: 0 auto;
+      width: 100%;
+      margin-top: 8px;
     }
+  }
+
+  @media screen and (max-width: 1100px) {
+    margin: 0 auto;
+  }
 
 `;
 export const profileDetailsTabTitle = css`
-    display:block;
-    @media screen and (max-width: 1100px) {
-        display:none;
-    }
+  display: block;
+  @media screen and (max-width: 1100px) {
+    display: none;
+  }
 `;
 export const changePasswordTitle = css`
-    display:block;
-    @media screen and (max-width: 1100px) {
-        display:none;
-    }
+  display: block;
+  @media screen and (max-width: 1100px) {
+    display: none;
+  }
 `;
