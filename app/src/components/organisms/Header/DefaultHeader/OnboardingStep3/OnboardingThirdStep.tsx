cfecd547e8a700f34900e9ae09b4a8e501abcb38
Kotti <christian@dualmeta.io>
2023-11-23 21:38:57 +0100

    added onboarding modal

--- app/src/components/organisms/Header/DefaultHeader/OnboardingStep3/OnboardingThirdStep.tsx
+++ app/src/components/organisms/Header/DefaultHeader/OnboardingStep3/OnboardingThirdStep.tsx
@@ -2,7 +2,6 @@ import { Search } from "@/components/Icons/Search";
 import OnboardingTutorialStep from "@/components/molecules/onboardingTutorialStep/OnboardingTutorialStep";
 import OnboardingTutorialStepItem from "@/components/molecules/onboardingTutorialStep/OnboardingTutorialStepItem";
 import OnboardingTutorialPopover from "@/components/organisms/onboardingTutorialPopover/OnboardingTutorialPopover";
-import useSetOnboardingResult from "@/hooks/useSetOnboardingResult";
 
 import { type Dispatch, type SetStateAction, type FunctionComponent } from "react";
 
@@ -15,8 +14,6 @@ type TOnboardingThirdStepProps = {
 
 const OnboardingThirdStep: FunctionComponent<TOnboardingThirdStepProps> = ({ onboardingStepsIndex, setOnboardingStepsIndex }) => 
 {
-  const { setOnboardingResult } = useSetOnboardingResult();
-
   return (
     <OnboardingTutorialPopover
       opened={onboardingStepsIndex === 2}
@@ -26,17 +23,15 @@ const OnboardingThirdStep: FunctionComponent<TOnboardingThirdStepProps> = ({ onb
           currentStep={3}
           totalSteps={3}
           stepTitle="Suche"
-          onNextPressHandler={() => 
-          {
-            setOnboardingResult({ result: "completed" });
-            setOnboardingStepsIndex(-1);
-          }}>
-          <OnboardingTutorialStepItem icon={<Search size={20}/>} itemTitle="Umfassende, blitzschnelle Suche" itemDescription="Dank ausgeklügelter Filtermöglichkeiten und neuester Technik findest du immer sofort deine benötigten Inhalte."/>
+          onNextPressHandler={() => setOnboardingStepsIndex(3)}>
+          <OnboardingTutorialStepItem
+            icon={<Search size={20}/>}
+            itemTitle="Umfassende, blitzschnelle Suche"
+            itemDescription="Dank ausgeklügelter Filtermöglichkeiten und neuester Technik findest du immer sofort deine benötigten Inhalte."
+          />
         </OnboardingTutorialStep>
       )}
-      popoverTarget={(
-        <HeaderItemSearchBar/>
-      )}
+      popoverTarget={(<HeaderItemSearchBar/>)}
     />
   );
 };
