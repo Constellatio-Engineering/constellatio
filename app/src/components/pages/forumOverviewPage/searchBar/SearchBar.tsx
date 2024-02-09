f0979a00ee5535699b783651e5e48167f1bbe809
Kotti <christian@dualmeta.io>
2024-02-09 21:28:19 +0100

    Search results styled

--- app/src/components/pages/forumOverviewPage/searchBar/SearchBar.tsx
+++ app/src/components/pages/forumOverviewPage/searchBar/SearchBar.tsx
@@ -1,8 +1,12 @@
+import { CustomLink } from "@/components/atoms/CustomLink/CustomLink";
+import Tag from "@/components/atoms/tag/Tag";
 import { Search } from "@/components/Icons/Search";
 import { useForumQuestionsSearchResults } from "@/hooks/useForumQuestionsSearchResults";
 import { useForumQuestionsSearchStore } from "@/stores/forumQuestionsSearch.store";
+import { appPaths } from "@/utils/paths";
 
 import { Input, Popover } from "@mantine/core";
+import Link from "next/link";
 import React, { type FunctionComponent } from "react";
 
 import * as styles from "./SearchBar.styles";
@@ -19,9 +23,10 @@ const SearchBar: FunctionComponent = () =>
     <Popover
       opened={isDrawerOpened && searchValue.length > 0}
       onChange={toggleDrawer}
+      zIndex={20}
       width={"100%"}
       offset={0}>
-      <form onSubmit={e => e.preventDefault()}>
+      <form css={styles.form} onSubmit={e => e.preventDefault()}>
         <Popover.Target>
           <Input
             data-autofocus
@@ -37,13 +42,18 @@ const SearchBar: FunctionComponent = () =>
           />
         </Popover.Target>
       </form>
-      <Popover.Dropdown css={styles.test}>
-        {searchResults?.hits.map((question, index) =>
-        {
-          return (
-            <p key={question.id}>{question.title}</p>
-          );
-        })}
+      <Popover.Dropdown css={styles.dropdown}>
+        <div css={styles.dropdownContentWrapper}>
+          {searchResults?.hits.map((question) => (
+            <Link key={question.id} href={`${appPaths.forum}/${question.id}`} css={styles.searchResult}>
+              <CustomLink styleType="link-content-title" component="p">
+                {question.title}
+              </CustomLink>
+              <Tag title={question.legalFieldName}/>
+            </Link>
+          ))}
+        </div>
+        <div css={styles.dropDownOverflowGradient}/>
       </Popover.Dropdown>
     </Popover>
   );
