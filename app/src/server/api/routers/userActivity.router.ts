671b3b09e2d7d68a796ee802f36d51192c507df5
Kotti <christian@dualmeta.io>
2024-05-10 18:11:46 +0200

    wip

--- app/src/server/api/routers/userActivity.router.ts
+++ app/src/server/api/routers/userActivity.router.ts
@@ -80,7 +80,7 @@ export const userActivityRouter = createTRPCRouter({
 
       const usageTimePerInterval = await usageTimeQuery;
 
-      console.log(usageTimePerInterval);
+      console.log(JSON.stringify(usageTimePerInterval, null, 2));
 
       return usageTimePerInterval;
     }),
