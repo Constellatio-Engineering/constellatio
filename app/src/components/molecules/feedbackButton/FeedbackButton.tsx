15a2230eccb09407aceb53b840b151b91759eb23
Kotti <christian@dualmeta.io>
2024-06-24 23:38:16 +0200

    potential formbricks fix

--- app/src/components/molecules/feedbackButton/FeedbackButton.tsx
+++ app/src/components/molecules/feedbackButton/FeedbackButton.tsx
@@ -1,89 +1,85 @@
 import { UnstyledButton } from "@/components/molecules/unstyledButton/UnstyledButton";
 import { env } from "@/env.mjs";
-import { supabase } from "@/lib/supabase";
 import { AuthStateContext } from "@/provider/AuthStateProvider";
-import { isProduction } from "@/utils/env";
 
 import { useTheme } from "@emotion/react";
 import formbricks from "@formbricks/js/app";
-import type { AuthChangeEvent, Session, Subscription } from "@supabase/gotrue-js";
-import React, {
-  type FunctionComponent, useCallback, useContext, useEffect, useRef
-} from "react";
+import React, { type FunctionComponent, useContext, useEffect, useRef } from "react";
 
 import * as styles from "./FeedbackButton.styles";
 
 const FeedbackButton: FunctionComponent = () =>
 {
   const theme = useTheme();
-  const { isUserLoggedIn } = useContext(AuthStateContext);
-  const authStateSubscriptionRef = useRef<{data: {subscription: Subscription}}>();
+  const authState = useContext(AuthStateContext);
+  const isUserLoggedIn = authState.isUserLoggedIn ?? false;
+  const { isLoading } = authState;
+  const user = authState.isUserLoggedIn ? authState.user : null;
   const hasFormbricksInitialized = useRef(false);
-
-  const onAuthStateChange = useCallback(async (event: AuthChangeEvent, session: Session | null): Promise<void> =>
-  {
-    switch (event)
-    {
-      case "INITIAL_SESSION":
-      case "SIGNED_IN":
-      {
-        const { email, id } = session?.user || {};
-
-        if(!id || !email || hasFormbricksInitialized.current)
-        {
-          break;
-        }
-
-        try
-        {
-          await formbricks.init({
-            apiHost: env.NEXT_PUBLIC_FORMBRICKS_HOST,
-            debug: true, // !isProduction,
-            environmentId: isProduction
-              ? env.NEXT_PUBLIC_FORMBRICKS_KEY_PRODUCTION
-              : env.NEXT_PUBLIC_FORMBRICKS_KEY_TESTINGS,
-            userId: id,
-          });
-
-          await formbricks.setEmail(email);
-          hasFormbricksInitialized.current = true;
-        }
-        catch (error)
-        {
-          console.log("Fehler beim initialisieren von Formbricks", error);
-        }
-        break;
-      }
-      case "SIGNED_OUT":
-      {
-        await formbricks.logout();
-        hasFormbricksInitialized.current = false;
-        break;
-      }
-      default:
-      {
-        break;
-      }
-    }
-  }, []);
 
   useEffect(() =>
   {
-    const currentSubscription = authStateSubscriptionRef.current?.data.subscription;
-    currentSubscription?.unsubscribe();
-
-    authStateSubscriptionRef.current = supabase.auth.onAuthStateChange(onAuthStateChange);
-
-    return () =>
+    if(isLoading)
     {
-      hasFormbricksInitialized.current = false;
-      currentSubscription?.unsubscribe();
+      return;
+    }
+
+    const initFormBricks = async (): Promise<void> =>
+    {
+      await formbricks.init({
+        apiHost: env.NEXT_PUBLIC_FORMBRICKS_HOST,
+        debug: true, // !isProduction,
+        environmentId: env.NEXT_PUBLIC_FORMBRICKS_ENVIRONMENT_ID,
+        userId: user?.id,
+      });
+
+      hasFormbricksInitialized.current = true;
     };
-  }, [onAuthStateChange]);
+
+    const setUserAttributes = async (): Promise<void> =>
+    {
+      const email = user?.email;
+      if(email) 
+      {
+        await formbricks.setEmail(email);
+      }
+    };
+
+    const logOut = async (): Promise<void> =>
+    {
+      // If the user is not signed in, we need to log out from Formbricks
+      // but this causes it to be uninitialized, so we need to init it again
+      await formbricks.logout();
+      await initFormBricks();
+    };
+
+    const configureFormbricks = async (): Promise<void> =>
+    {
+      if(!hasFormbricksInitialized.current)
+      {
+        await initFormBricks();
+      }
+
+      if(hasFormbricksInitialized.current)
+      {
+        if(isUserLoggedIn)
+        {
+          await setUserAttributes();
+        }
+        else
+        {
+          await logOut();
+        }
+      }
+    };
+
+    configureFormbricks().catch((error) => console.error("Error configuring Formbricks", error));
+
+  }, [user, isLoading, isUserLoggedIn]);
 
   return (
     <UnstyledButton
-      styles={styles.feedbackButtonStyles(isUserLoggedIn ?? false, theme)}
+      styles={styles.feedbackButtonStyles(isUserLoggedIn, theme)}
       onClick={() => formbricks.track("feedback_button_clicked")}>
       Feedback
     </UnstyledButton>
--- app/src/provider/AuthStateProvider.tsx
+++ app/src/provider/AuthStateProvider.tsx
@@ -1,18 +1,21 @@
-import { useSession } from "@supabase/auth-helpers-react";
+import { useSessionContext } from "@supabase/auth-helpers-react";
 import { type Session, type User } from "@supabase/supabase-js";
 import {
   createContext, type FunctionComponent, type ReactNode, useMemo
 } from "react";
 
 type InitialAuthState = {
+  isLoading: boolean;
   isUserLoggedIn: null;
 };
 
 type UserNotLoggedInState = {
+  isLoading: boolean;
   isUserLoggedIn: false;
 };
 
 type UserLoggedInState = {
+  isLoading: boolean;
   isUserLoggedIn: true;
   session: Session;
   user: User;
@@ -21,23 +24,26 @@ type UserLoggedInState = {
 type AuthStateContext = UserNotLoggedInState | UserLoggedInState | InitialAuthState;
 
 const initialAuthState: InitialAuthState = {
+  isLoading: true,
   isUserLoggedIn: null
 };
 
 export const AuthStateContext = createContext<AuthStateContext>(initialAuthState);
 
-export const getIsUserLoggedInClient = (session: Session | null): UserNotLoggedInState | UserLoggedInState =>
+export const getIsUserLoggedInClient = (session: Session | null, isLoading: boolean): UserNotLoggedInState | UserLoggedInState =>
 {
   const isUserLoggedIn = session != null;
 
   if(!isUserLoggedIn)
   {
     return ({
+      isLoading,
       isUserLoggedIn: false
     });
   }
 
   return ({
+    isLoading,
     isUserLoggedIn: true,
     session,
     user: session.user
@@ -50,8 +56,8 @@ type AuthStateProviderProps = {
 
 const AuthStateProvider: FunctionComponent<AuthStateProviderProps> = ({ children }) =>
 {
-  const session = useSession();
-  const memoizedContext: AuthStateContext = useMemo(() => getIsUserLoggedInClient(session), [session]);
+  const { isLoading, session } = useSessionContext();
+  const memoizedContext: AuthStateContext = useMemo(() => getIsUserLoggedInClient(session, isLoading), [session, isLoading]);
 
   return (
     <AuthStateContext.Provider value={memoizedContext}>
