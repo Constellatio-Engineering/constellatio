fd00e7f7eaa9eaa4904e47397c7d7b9f3493d893
Kotti <christian@dualmeta.io>
2024-07-31 05:49:26 +0200

    added script to enable auto tax for all existing customers
    should fix the issue where the umsatzsteuer is not on the invoices

--- app/src/pages/api/cron/sync-users-to-clickup.ts
+++ app/src/pages/api/cron/sync-users-to-clickup.ts
@@ -9,7 +9,6 @@ import { updateClickupTask } from "@/lib/clickup/tasks/update-task";
 import { type ClickupTask } from "@/lib/clickup/types";
 import { clickupCrmCustomField, clickupRequestConfig, getUserCrmData } from "@/lib/clickup/utils";
 import { stripe } from "@/lib/stripe";
-import { type Nullable } from "@/utils/types";
 import { sleep } from "@/utils/utils";
 
 import { createPagesServerClient, type SupabaseClient } from "@supabase/auth-helpers-nextjs";
@@ -272,7 +271,19 @@ const handler: NextApiHandler = async (req, res): Promise<void> =>
     }
   }
 
-  console.log("Finished creating new users");
+  for(let i = 0; i < newUsers.length; i++)
+  {
+    const user = newUsers[i]!;
+    await createClickupTask(env.CLICKUP_CRM_LIST_ID, user.crmData);
+
+    if(i % 50 === 0)
+    {
+      console.log(`Created ${i} new users - Pause`);
+      await sleep(61000);
+    }
+  }
+
+  const createNewUsersResults = await Promise.allSettled(newUsers.map(async ({ crmData }) => createClickupTask(env.CLICKUP_CRM_LIST_ID, crmData)));
 
   return res.status(200).json({ message: "Finished" });
 };
