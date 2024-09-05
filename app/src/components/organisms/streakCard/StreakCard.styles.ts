65ba2cab257cd4b20c0f4dd4f3726adf6c54f702
PHBear <p.hartenbauer@gmx.de>
2024-09-05 12:07:55 +0200

    added tooltip and fire emoji to streak

--- app/src/components/organisms/streakCard/StreakCard.styles.ts
+++ app/src/components/organisms/streakCard/StreakCard.styles.ts
@@ -1,22 +1,29 @@
-import { css } from "@emotion/react";
-import { type MantineTheme } from "@mantine/styles";
+import { colors } from "@/constants/styles/colors";
 
-export const wrapper = (theme: MantineTheme) => css`
+import { css } from "@emotion/react";
+
+export const wrapper = css`
   display: flex;
   flex-direction: column;
   gap: 2px;
-  background-color: ${theme.colors["neutrals-01"][0]};
+  background-color: ${colors["neutrals-01"][0]};
   width: fit-content;
   border-radius: 12px;
   padding: 9px 18px 18px 18px;
 `;
 
-export const streakWrapper = (theme: MantineTheme) => css`
+export const streakWrapper = css`
   display: flex;
   flex-direction: row;
 `;
 
-export const feuerWrapper = (theme: MantineTheme) => css`
+export const headerWrapper = css`
+  display: flex;
+  flex-direction: row;  
+  gap: 2px;
+`;
+
+export const feuerWrapper = css`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -24,36 +31,48 @@ export const feuerWrapper = (theme: MantineTheme) => css`
   padding-right: 12px;
 `;
 
-export const dayFireWrapper = (theme: MantineTheme) => css`
+export const dayFireWrapper = css`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 8px;
 `;
 
-export const streakNumberWrapper = (theme: MantineTheme) => css`
+export const streakNumberWrapper = css`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
-  border-left: 1px solid ${theme.colors["neutrals-01"][7]};
+  border-left: 1px solid ${colors["neutrals-01"][7]};
   padding-left: 12px;
 `;
 
-export const streakText = (theme: MantineTheme) => css`
+export const fireEmojiWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-left: 6px;
+`;
+
+export const streakText = css`
   font-size: 28px;
 `;
 
-export const streakTextSub = (theme: MantineTheme) => css`
+export const streakTextSub = css`
   font-size: 16px;
 `;
 
-export const dayTextGrey = (theme: MantineTheme) => css`
-  color: ${theme.colors["neutrals-01"][5]};
+export const dayTextGrey = css`
+  color: ${colors["neutrals-01"][5]};
 `;
 
-export const headerText = (theme: MantineTheme) => css`
+export const headerText = css`
   font-size: 18px;
   padding-bottom: 6px;
   font-weight: 700;
+`;
+
+export const fireEmoji = css`
+  font-size: 45px;
 `;
--- app/src/components/organisms/streakCard/StreakCard.tsx
+++ app/src/components/organisms/streakCard/StreakCard.tsx
@@ -1,7 +1,10 @@
 import { BodyText } from "@/components/atoms/BodyText/BodyText";
 import { FireIcon } from "@/components/Icons/Fire";
+import { InfoFilled } from "@/components/Icons/InfoFilled";
+import { colors } from "@/constants/styles/colors";
 import { api } from "@/utils/api";
 
+import { Tooltip } from "@mantine/core";
 import { type FunctionComponent } from "react";
 
 import * as styles from "./StreakCard.styles";
@@ -15,7 +18,7 @@ const StreakCard: FunctionComponent = () =>
     refetchOnWindowFocus: false,
   });
 
-  const days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"];
+  const days = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"] as const;
   const currentDay = new Date().getDay();
   const orderedDays = [...days.slice(currentDay), ...days.slice(0, currentDay)];
   const finalWeekStreak = orderedDays.map((day) => { return { day, isSatisfied: false }; });
@@ -28,7 +31,7 @@ const StreakCard: FunctionComponent = () =>
       {
         if(weekStreak[i] !== undefined) 
         {
-          finalWeekStreak[i].isSatisfied = weekStreak[i] ?? false;
+          finalWeekStreak[i]!.isSatisfied = weekStreak[i] ?? false;
         }
       }
     }
@@ -46,14 +49,22 @@ const StreakCard: FunctionComponent = () =>
 
   return (
     <div css={styles.wrapper}>
-      <div>
+      <div css={styles.headerWrapper}>
         <BodyText styleType="body-01-regular" css={styles.headerText}>Lernstreak</BodyText>
+        <Tooltip 
+          label={`Lernstreak – Deine Lernstreak wird aktiviert, wenn du an einem Tag mindestens eine Stunde lernst, 
+          einen Fall löst oder im Forum eine Frage stellst oder beantwortest. Du musst nicht jeden Tag aktiv sein, 
+          um deine Lernstreak zu halten. In einem Zeitraum von sieben Tagen musst du jedoch an fünf Tagen eine dieser 
+          Aktivitäten erledigen. Nicht genutzte Pausetage aus früheren Wochen werden in die nächste Woche übernommen. 
+          Halte deine Lernstreak so lange wie möglich aufrecht, um dir besondere Badges zu verdienen.`}>
+          <InfoFilled size={14}/>
+        </Tooltip>
       </div>
       <div css={styles.streakWrapper}>
         <div css={styles.feuerWrapper}>
           {finalWeekStreak.map((satisfactionObj, index) => (
             <div key={index} css={styles.dayFireWrapper}>
-              <FireIcon size={20} color={satisfactionObj.isSatisfied ? "#A90000" : "#D3D3D3"}/>
+              <FireIcon size={20} color={satisfactionObj.isSatisfied ? colors["brand-01"][4] : "#D3D3D3"}/>
               {(() => 
               {
                 const isGreyText = !isStreakPending && (streak?.numStreakDays ?? 0) < 7 && index < (7 - (streak?.numStreakDays ?? 0));
@@ -70,14 +81,16 @@ const StreakCard: FunctionComponent = () =>
           {!isStreakPending && streak && (
             <BodyText styleType="body-01-regular" css={styles.streakText}>{streak.numStreakDays ?? 0}</BodyText> 
           )}
-          {!isStreakPending && streak && streak.numStreakDays === 1 ? (<span>Tag</span>) : (<span>Tage</span>)}
+          {!isStreakPending && streak && (streak.numStreakDays === 1 ? (<span>Tag</span>) : (<span>Tage</span>))}
           {!isStreakPending && !streak && (
             <>
               <BodyText styleType="body-01-regular" css={styles.streakText}>0</BodyText>
               <span>Tage</span>
             </>
           )}
-
+        </div>
+        <div css={styles.fireEmojiWrapper}>
+          {!isStreakPending && streak && streak.numStreakDays >= 5 && (<span css={styles.fireEmoji}>🔥</span>)}
         </div>
       </div>
     </div>
