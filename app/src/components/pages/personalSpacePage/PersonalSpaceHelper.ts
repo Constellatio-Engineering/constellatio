81977617f12ce8b37ead9d6388e4c4420266dc40
Kotti <christian@dualmeta.io>
2024-09-21 23:19:55 +0200

    resolved merge conflicts

--- app/src/components/pages/personalSpacePage/PersonalSpaceHelper.ts
+++ app/src/components/pages/personalSpacePage/PersonalSpaceHelper.ts
@@ -1,32 +1,23 @@
 import type { GetOverviewPagePropsResult } from "@/services/content/getOverviewPageProps";
 import { type IGenAsset } from "@/services/graphql/__generated/sdk";
 
-export const categoriesHelper = (favCategoryData: {
-  BookmarkIconSvg: IGenAsset;
-  FavCategoryId: string;
-  bookmarkedCasesLength: number;
-  slug: string;
-}, materialsCategoryData: {
-  FileIconSvg: IGenAsset;
-  MaterialsCategoryId: string;
-  slug: string;
-  uploadedFilesLength: number;
-}): GetOverviewPagePropsResult["allMainCategories"] =>
+export const categoriesHelper = (
+  materialsCategoryData: {
+    FileIconSvg: IGenAsset;
+    MaterialsCategoryId: string;
+    slug: string;
+    uploadedFilesLength: number;
+  },
+  favCategoryData: {
+    BookmarkIconSvg: IGenAsset;
+    FavCategoryId: string;
+    bookmarkedCasesLength: number;
+    slug: string;
+  },
+): GetOverviewPagePropsResult["allMainCategories"] =>
 {
   return [
     {
-      __typename: "MainCategory",
-      icon: {
-        src: favCategoryData?.BookmarkIconSvg.src,
-        title: "bookmark-icon"
-      },
-      
-      id: favCategoryData?.FavCategoryId,
-      //   casesPerCategory: favCategoryData?.bookmarkedCases?.length ?? 0,
-      itemsPerCategory: favCategoryData?.bookmarkedCasesLength ?? 0,
-      mainCategory: "Favoriten",
-      slug: favCategoryData.slug
-    }, {
       __typename: "MainCategory",
       icon: {
         src: materialsCategoryData?.FileIconSvg.src,
@@ -36,6 +27,17 @@ export const categoriesHelper = (favCategoryData: {
       itemsPerCategory: materialsCategoryData?.uploadedFilesLength ?? 0,
       mainCategory: "Deine Dateien",
       slug: materialsCategoryData.slug
-    }
+    },
+    {
+      __typename: "MainCategory",
+      icon: {
+        src: favCategoryData?.BookmarkIconSvg.src,
+        title: "bookmark-icon"
+      },
+      id: favCategoryData?.FavCategoryId,
+      itemsPerCategory: favCategoryData?.bookmarkedCasesLength ?? 0,
+      mainCategory: "Favoriten",
+      slug: favCategoryData.slug
+    },
   ];
 };
