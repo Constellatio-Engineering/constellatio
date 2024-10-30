3ae39654ac307d00ea637fe861eea3229462dd98
Kotti <christian@dualmeta.io>
2024-10-30 17:59:34 +0100

    wip

--- workspace/app/next.config.mjs
+++ workspace/app/next.config.mjs
@@ -8,6 +8,12 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
+  compiler: {
+    emotion: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     // bundlePagesExternals: process.env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT !== "development", currently not working because of mantine
     optimizePackageImports: [
@@ -16,60 +22,7 @@ const config = {
       "@tiptap/*"
     ],
   },
-  images:{
-    remotePatterns: [
-      {
-        hostname: "assets.caisy.io",
-        protocol: "https",
-        port: '',
-      },
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-        port: '',
-      },
-      {
-        protocol: "https",
-        hostname: "via.placeholder.com",
-        port: '',
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: '',
-      },
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-        port: '',
-      },
-    ],
-  },
-  reactStrictMode: true,
-  compiler: {
-    emotion: true,
-  },
-  logging: {
-    fetches: {
-      fullUrl: false
-    }
-  },
-  productionBrowserSourceMaps: process.env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT !== "production",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  i18n: {
-    /**
-     * Caution: If you change this, you need apply the same changes in `next-i18next.config.js` as well, as this is duplicated there for now.
-     * There is currently no workaround for this since `next-i18next` does not support esm.
-     */
-    defaultLocale: defaultLocale,
-    locales: [defaultLocale],
-  },
-  swcMinify: true,
+  // eslint-disable-next-line @typescript-eslint/require-await
   async headers()
   {
     return [
@@ -84,6 +37,54 @@ const config = {
         source: "/:all*(svg|jpg|png)"
       }
     ];
+  },
+  i18n: {
+    /**
+     * Caution: If you change this, you need apply the same changes in `next-i18next.config.js` as well, as this is duplicated there for now.
+     * There is currently no workaround for this since `next-i18next` does not support esm.
+     */
+    defaultLocale,
+    locales: [defaultLocale],
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "assets.caisy.io",
+        port: "",
+        protocol: "https", 
+      },
+      {
+        hostname: "storage.googleapis.com",
+        port: "",
+        protocol: "https",
+      },
+      {
+        hostname: "via.placeholder.com",
+        port: "",
+        protocol: "https",
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        protocol: "https",
+      },
+      {
+        hostname: "media.licdn.com",
+        port: "",
+        protocol: "https",
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: false
+    }
+  },
+  productionBrowserSourceMaps: process.env.NEXT_PUBLIC_DEPLOYMENT_ENVIRONMENT !== "production",
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true
   },
 };
 
