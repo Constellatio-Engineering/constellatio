edb79383af9350ccbd6463bd991769c897025b10
Kotti <christian@dualmeta.io>
2024-11-02 01:57:17 +0100

    fixed import drom drizzle not working

--- workspace/packages/db/src/index.ts
+++ workspace/packages/db/src/index.ts
@@ -1,8 +1,10 @@
 /* eslint-disable barrel-files/avoid-re-export-all,barrel-files/avoid-barrel-files */
 
-// exporting everything from drizzle-orm is correct and needed here because every package needs to import the same drizzle package, because of the type inference
+// expand this list if you need more exports. Somehow exporting all with * causes error '... us not a function'
+export {
+  eq, isNull, or, and, sql, desc, count, gt, lt, lte, gte, inArray, min, max, sum, notInArray, not, ne, SQL, asc, type SQLWrapper, between, countDistinct
+} from "drizzle-orm/sql";
 
-export * from "drizzle-orm/sql";
 export { alias } from "drizzle-orm/pg-core";
 export { getTableColumns } from "drizzle-orm/utils";
 export { drizzle as drizzleServerless } from "drizzle-orm/neon-serverless";
