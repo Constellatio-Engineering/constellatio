389fe574021f6e6b78a25b5bafe4b2f259b5d842
Noah <protected.noah.e@gmail.com>
2024-12-05 14:04:12 +0100

    WIP: Unit Information Card

--- workspace/app/src/components/pages/learningPathDetails/learningPathHeader/LearningPathHeader.tsx
+++ workspace/app/src/components/pages/learningPathDetails/learningPathHeader/LearningPathHeader.tsx
@@ -6,8 +6,13 @@ import { type FunctionComponent } from "react";
 
 import * as sharedStyles from "../LearningPathDetails.styles";
 import * as styles from "./LearningPathHeader.styles";
+import { IconClock } from "@tabler/icons-react";
+import { Unit } from "@/components/Icons/Unit";
 
-type Props = Pick<IGenLearningPath, "description" | "estimatedDuration" | "title"> & {
+type Props = Pick<
+  IGenLearningPath,
+  "description" | "estimatedDuration" | "title"
+> & {
   readonly totalTasks: number;
 };
 
@@ -16,15 +21,39 @@ export const LearningPathHeader: FunctionComponent<Props> = ({
   estimatedDuration,
   title,
   totalTasks,
-}) =>
-{
+}) => {
   return (
     <div css={[sharedStyles.card, styles.wrapper]}>
-      <Title order={1}>{title}</Title>
-      <Richtext data={description}/>
       <div>
-        <p>Duration: {estimatedDuration} Minuten</p>
-        <p>Aufgaben: {totalTasks}</p>
+        <div css={styles.UnitIconAndTotalTaskWrapper}>
+          <Unit />
+          <div css={styles.totalTasksNotificationWrapper}>
+            <span css={styles.totalTasksNotification}>{totalTasks}</span>
+          </div>
+        </div>
+      </div>
+
+      <Title order={1}>{title}</Title>
+
+      <Richtext data={description} />
+
+      <div css={styles.metricsWrapper}>
+        <div css={styles.metricItem}>
+          <div css={styles.metricIcon}>
+            <IconClock size={24} />
+          </div>
+          <div css={styles.metricText}>
+            <span css={styles.metricLabel}>Duration</span>
+            <span css={styles.metricValue}>{estimatedDuration}</span>
+          </div>
+        </div>
+        <div css={styles.metricItem}>
+          <div css={styles.metricIcon}>?</div>
+          <div css={styles.metricText}>
+            <span css={styles.metricLabel}>Practice</span>
+            <span css={styles.metricValue}>{totalTasks}</span>
+          </div>
+        </div>
       </div>
     </div>
   );
