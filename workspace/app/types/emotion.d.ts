b3b708f7e7b3ce96b97613a10dfbac5cb7bc5716
Kotti <christian@dualmeta.io>
2024-12-11 02:54:55 +0100

    made app router work

--- workspace/app/types/emotion.d.ts
+++ workspace/app/types/emotion.d.ts
@@ -1,6 +1,15 @@
 import '@emotion/react';
 import type { MantineTheme } from '@mantine/core';
+import {} from 'react'
+import { Interpolation } from '@emotion/serialize'
+import { Theme } from '@emotion/react'
 
 declare module '@emotion/react' {
   export interface Theme extends MantineTheme {}
+}
+
+declare module 'react' {
+  interface Attributes {
+    css?: Interpolation<Theme>
+  }
 }
